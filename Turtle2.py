fc36d94cad36affaf57491f277e40d8e1915d0b9
Sakshi Chilkoti <145189160+SakshiChilkoti@users.noreply.github.com>
2024-05-16 21:22:47 +0530

    Add files via upload

---
+++ Turtle2.py
@@ -0,0 +1,13 @@
+import turtle 
+import time
+turtle.forward(100)
+turtle.left(90)
+time.sleep(1)
+turtle.forward(100)
+turtle.left(90)
+time.sleep(1)
+turtle.forward(100)
+turtle.left(90)
+time.sleep(1)
+turtle.forward(100)
+turtle.left(90)
---
+++ Turtle3.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+turtle.color("red")
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
